061d960c55be06e2b5788ac5df2783cf99390696
sumit sahni <3075sumitsahni@gmail.com>
2022-03-04 10:33:13 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import styled from "styled-components";
+import Nav from './Components/Navbar';
+
+const Dashboard = styled.div`
+
+
+`
+
+
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hello GIhub</h1>
-    </div>
+      <Dashboard className='container-fluid p-5'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-lg-2 col-12'>
+            <Nav/>
+            </div>
+            <div className='col-lg-10 col-12 border border-dark'>
+              <h1>Dashboard</h1>
+            </div>
+          </div>
+       
+        </div>
+        
+      </Dashboard>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,76 @@
+import React from "react";
+import styled from "styled-components";
+import { SiAngellist} from "react-icons/si";
+import { GoDashboard } from "react-icons/go";
+import { RiExchangeDollarLine} from "react-icons/ri";
+import { MdOutlineNotifications } from "react-icons/md";
+import { MdLogout } from "react-icons/md";
+
+
+
+const Main  = styled.div`
+  display:flex;
+  flex-direction:column;
+  flex-wrap:wrap;
+  justify-content:space-between;
+  align-items:center;
+//   border:1px solid black;
+  height:90vh;
+  opacity:0.5;
+  width:auto;
+  height:80vh;
+
+  @media (max-width: 968px) {
+    flex-direction:row;
+    flex-wrap:wrap;
+    webkit-justify-content:none;
+    height:auto;
+    position:relative;
+     
+  }
+`
+const Div1 = styled.div`
+    display:flex;
+    flex-direction:column;
+    flex-wrap:wrap;
+    align-items:center;
+    justify-content:center;
+   @media (max-width: 968px) {
+    flex-direction:row!important;
+
+   }
+`
+const Icon = styled.div`
+  font-size:2.5rem;
+  cursor:pointer;
+  display:flex;
+  flex-direction:column;
+  flex-wrap:wrap;
+  align-items:center;
+  padding:1rem;
+  @media (max-width: 1450px) {
+    font-size:1rem;
+    padding:1rem;
+    h6{
+        display:none;
+    }
+   }
+`
+const Nav = () => {
+    return (
+       <Main>
+             <Div1>
+                 <Icon><GoDashboard /><h6>Dashboard</h6></Icon>
+                 <Icon><SiAngellist /><h6>Analytics</h6></Icon>
+                 <Icon><RiExchangeDollarLine /><h6>Trading</h6></Icon>
+                 <Icon><MdOutlineNotifications /><h6>Icon</h6></Icon>
+             </Div1>
+             <div>
+             <Icon><MdLogout /><h6>Logout</h6></Icon>
+              
+             </div>
+       </Main>
+    );
+}
+
+export default Nav;
